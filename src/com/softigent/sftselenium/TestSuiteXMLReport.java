7c884689f4d92c4946e8aeb68c66b7fa1c10f4fd
David Gofman <dgofman@gmail.com>
2019-08-27 16:10:22 -0700

    update code

--- src/com/softigent/sftselenium/TestSuiteXMLReport.java
+++ src/com/softigent/sftselenium/TestSuiteXMLReport.java
@@ -30,18 +30,20 @@ public class TestSuiteXMLReport implements ITestSuiteReport {
 		writer.println("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
 	}
 
-
 	@Override
 	public void openBody(TestRunnerInfo info) {
 		writer.println("<testsuites>");
 	}
 
+	public void addProperties(TestRunnerInfo info, Date startTime) {
+		writer.println("    <property name=\"description\" value=\"" + info.getDescription() + "\" />");
+		writer.println("    <property name=\"start_time\" value=\"" + DateFormat.getDateTimeInstance().format(startTime) + "\" />");
+	}
+
 	@Override
 	public void addHeader(TestRunnerInfo info, Date startTime) {
 		writer.println("  <properties>");
-		writer.println("    <property name=\"description\" value=\"" + info.getDescription() + "\" />");
-		writer.println("    <property name=\"start_time\" value=\"" + DateFormat.getDateTimeInstance().format(startTime)
-				+ "\" />");
+		addProperties(info, startTime);
 		writer.println("  </properties>");
 		writer.flush();
 	}
@@ -53,9 +55,10 @@ public class TestSuiteXMLReport implements ITestSuiteReport {
 
 	@SuppressWarnings("deprecation")
 	@Override
-	public void addTest(TestRunnerInfo info, long time, List<Failure> failures, boolean ignored, Description description) {
- 		results.add("      <testcase classname=\"" + description.getClassName() + "\" name=\""
-				+ description.getMethodName() + "\" time=\"" + ((float)time) / 1000 + "\">");
+	public void addTest(TestRunnerInfo info, long time, List<Failure> failures, boolean ignored,
+			Description description) {
+		results.add("      <testcase classname=\"" + description.getClassName() + "\" name=\""
+				+ description.getMethodName() + "\" time=\"" + ((float) time) / 1000 + "\">");
 		if (ignored) {
 			results.add("        <skipped />");
 		}
@@ -68,9 +71,11 @@ public class TestSuiteXMLReport implements ITestSuiteReport {
 				msg = "";
 			}
 			if (exception instanceof AssertionFailedError || exception instanceof AssertionError) {
-				results.add("        <failure " + msg + "type=\"" + exception.getClass().getTypeName() + "\"><![CDATA[" + failure.getTrace() + "]]></failure>");
+				results.add("        <failure " + msg + "type=\"" + exception.getClass().getTypeName() + "\"><![CDATA["
+						+ failure.getTrace() + "]]></failure>");
 			} else {
-				results.add("        <error " + msg + "type=\"" + exception.getClass().getTypeName() + "\"><![CDATA[" + failure.getTrace() + "]]></error>");
+				results.add("        <error " + msg + "type=\"" + exception.getClass().getTypeName() + "\"><![CDATA["
+						+ failure.getTrace() + "]]></error>");
 			}
 		}
 		results.add("      </testcase>");
@@ -86,7 +91,7 @@ public class TestSuiteXMLReport implements ITestSuiteReport {
 		writer.println("  <testsuite errors=\"" + errors.size() + "\" failures=\"" + asserts.size() + "\" skipped=\""
 				+ result.getIgnoreCount() + "\" tests=\"" + result.getRunCount() + "\" name=\""
 				+ testCase.getSimpleName() + "\" package=\"" + testCase.getPackage() + "\" time=\""
-				+ ((float)time) / 1000 + "\" timestamp=\""
+				+ ((float) time) / 1000 + "\" timestamp=\""
 				+ new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date()) + "\">");
 		for (String testcase : results) {
 			writer.println(testcase);
