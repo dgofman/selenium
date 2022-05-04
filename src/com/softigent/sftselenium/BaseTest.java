fe2a39a6fcea54e493e9f537a2e096bdc54aa6ca
David Gofman <dgofman@gmail.com>
2022-05-04 15:20:10 -0700

    version 8.6.0

--- src/com/softigent/sftselenium/BaseTest.java
+++ src/com/softigent/sftselenium/BaseTest.java
@@ -48,7 +48,7 @@ public abstract class BaseTest {
 	
 	protected CloseDriver doCloseDriver;
 	
-	public static final double GIT_VERSION = 8.5;
+	public static final double GIT_VERSION = 8.6;
 	public static final int GIT_PATCH = 0;
 
 	@Rule
